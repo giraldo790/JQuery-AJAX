2a935b88d7766c22ab4366d948b695569de524bf
Esteban Giraldo <giraldo790@hotmail.com>
2022-03-05 23:41:00 -0500

    second actualization

--- AJAX/js/script2.js
+++ AJAX/js/script2.js
@@ -1,6 +1,39 @@
-$('.item-box').on('click', 'a.info-link', function(event) {
+$.ajax('/AJAX/js/destinations.json', {
+    dataType: 'json',
+    contentTyoe: 'application/json',
+    cache: false
+})
+.done(function(response) {
+    var html;
+    $.each(response, function(index, element) {
+        html = '<div class="item-box" data-id="' + element.id + '">';
+        html += '<img src="/img/' + element.image + '">';
+        html += '<div class="item-tittle">' + element.name + '</div>';
+        html += '<p>' + element.description + '</p>';
+        html += '<div class="item-price">' + element.price + '</div>';
+        html += '<button>Add to cart</button>';
+        html += '<div><a href="#" class="info-link">More info</a></div>';
+        html += '<div class="more-info"><p>' + element.moreInfo + '</p></div>';
+        html += '</div>';
+        $('body').append(html);
+    });
+})
+$('body').on('click', '.info-link', function(event) {
     event.preventDefault();
-    $(this).closest('.item-box').find('.more-info').toggle(800); //alterna entre .show() y .hide()
-    // $(this).closest('.item-box').find('.more-info').fadeToggle(3000); //alterna entre fadeIn() y fadeOut()
-    // $(this).closest('.item-box').find('.more-info').slideToggle(1000); //alterna entre slideDown() y slideUp()
+    $(this).closest('.item-box').find('.more-info').slideToggle(800);
+})
+var cart = 0;
+$('body').on('click', '.item-box button', function(event) {
+    event.preventDefault();
+    var id = +$(this).closest('.item-box').data('id');
+    $.ajax('/AJAX/js/addItem.json', {
+        data: {id: id},
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json', 
+    })
+    .done(function(response) {
+        cart += response.price;
+        $('.shopping-cart').text('Total $' + cart);
+    });
 })
